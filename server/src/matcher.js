ee73e0e0cd22327c9a20251a760b5e6e9fa42286
Nicky van Urk <nickyvanurk@gmx.com>
2020-04-29 16:47:52 +0200

    Add matchmaking functionality

--- server/src/matcher.js
+++ server/src/matcher.js
@@ -11,23 +11,48 @@ export default class Matcher {
     const id = uuidv4();
     const session = { id, ws };
 
-    console.log(`Registering ${id}`);
-
     this.sessions[id] = session;
+    this.tryMatch(session);
 
     ws.on('close', () => this.unregister(id));
     ws.on('error', () => this.unregister(id));
     ws.on('message', (data) => this.handleMessage(id, data.toString()));
   }
 
-  unregister(id) {
-    console.log(`Unregistering ${id}`);
+  tryMatch(session) {
+    if (this.unmatched.length > 0) {
+      const other = this.sessions[this.unmatched.shift()];
 
+      if (other) {
+        session.peer = other.id;
+        other.peer = session.id;
+
+        this.send(session, {type: 'matched', match: other.id});
+        this.send(other, {type: 'matched', match: session.id});
+
+        console.log(`Matching ${session.id} with ${other.id}`);
+      }
+    } else {
+      this.unmatched.push(session.id);
+    }
+  }
+
+  unregister(id) {
     this.unmatched = this.unmatched.filter(other => id !== other);
     delete this.sessions[id];
   }
 
   handleMessage(id, message) {
+    console.log(`Message from ${id}: ${message}`);
+  }
 
+  send(session, payload) {
+    try {
+      if (session.ws.readyState === WebSocket.OPEN) {
+        session.ws.send(JSON.stringify(payload));
+      }
+    } catch (error) {
+      console.error(`Error sending to ${session.id}`);
+    }
   }
 }
